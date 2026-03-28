986e81d330a485b6338017cc42e4950e7707edaa
roadrunner0-0 <avibatra360@gmail.com>
2026-03-27 23:59:59 -0700

    Adding else statement to simple_calculator

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -39,6 +39,10 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
             return num1 / num2
         else:
             raise ValueError("Cannot divide by zero.")
+    else:
+        raise ValueError("Invalid operation." 
+        "Please choose from 'add', 'subtract', " 
+        "'multiply', or 'divide'.")
 
 
 def main():
