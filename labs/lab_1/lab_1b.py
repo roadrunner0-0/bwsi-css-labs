130eff570fd11f854283e7756921cd25305b929e
roadrunner0-0 <avibatra360@gmail.com>
2026-03-27 23:12:10 -0700

    Fixed character lengths

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -1,7 +1,8 @@
 """
 lab_1b.py
 
-This is a script that implements a simple calculator. It takes two numbers and an operation,
+This is a script that implements a simple calculator.
+It takes two numbers and an operation,
 then performs the operation and returns the result. 
 
 The script asks the user to input the numbers and the operation to be performed,
@@ -14,11 +15,12 @@ import sys
 
 def simple_calculator(operation: str, num1: float, num2: float) -> float:
     """
-    Function that takes in two numbers and an operation (add, subtract, multiply, divide),
+    Function that takes in two numbers and an operation,
     then performs the operation on the two numbers and returns the result.
 
     Args:
-        operation (str): The operation to perform ("add", "subtract", "multiply", "divide").
+        operation (str): The operation to perform
+        ("add", "subtract", "multiply", "divide").
         num1 (float): The first number.
         num2 (float): The second number.
 
@@ -41,7 +43,7 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
 
 def main():
     
-    print(f"===== Simple Calculator =====")
+    print("===== Simple Calculator =====")
 
     # Ask the user for sample input    
     num1 = input("Enter the first number: ")
@@ -54,11 +56,15 @@ def main():
     try:
         num2 = float(num2)
     except ValueError:
-        print("Invalid input. Please enter a valid number.\n")
+        print(
+            "Invalid input. Please enter a valid number.\n")
         main()  # Restart the main function to get valid input
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+    operation = input(
+        "Enter the operation (add, subtract, multiply, divide):"
+        ).strip().lower()
     if operation not in ["add", "subtract", "multiply", "divide"]:
-        print("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
+        print("Invalid operation. Please choose from"
+              " 'add', 'subtract', 'multiply', or 'divide'.")
     
         main()  # Restart the main function to get valid input
     # Perform the calculation and display the result
